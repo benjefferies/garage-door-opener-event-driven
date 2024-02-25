061b242f243331941a7daed3c8ca8322161f464f
Deanna Trace <deannagtrace@hotmail.com>
2024-02-25 08:54:34 +0000

    Refactor GPIO import and add button detection

--- backend/index.ts
+++ backend/index.ts
@@ -1,7 +1,8 @@
 import Pusher from 'pusher-js';
 import dotenv from 'dotenv';
-const Gpio = require('onoff').Gpio;
+import { Gpio } from 'onoff';
 const relay = new Gpio(14, 'out');
+const detect = new Gpio(4, 'in', 'both');
 
 dotenv.config();
 
@@ -14,10 +15,20 @@ const pusher = new Pusher(process.env.PUSHER_APP_KEY, {
 });
 
 process.on('SIGINT', _ => {
+  detect.unexport();
   relay.unexport();
 });
 
 pusher.subscribe('garage-door').bind('toggle', (data: Message) => {
   console.log('Received toggle:', data.message);
   relay.writeSync(relay.readSync() === 0 ? 1 : 0);
+});
+
+
+detect.watch((err, value) => {
+  if (err) {
+    throw err;
+  }
+
+  console.log('Button pressed:', value);
 });
